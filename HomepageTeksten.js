76148d031b535ea5fc60b9e961ffbcb05fe65eb0
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2024-01-29 21:27:19 +0100

    feat: nieuws :magic:

---
+++ HomepageTeksten.js
@@ -0,0 +1,18 @@
+const nieuws1 = `
+### Gelekte beelden van NASA bewijzen dat de aarde plat is
+**Datum**: 20 januari 2024  
+Er zou een simulatie zijn gelekt van NASA waarin te zien is hoe de aarde werd ingesloten door een gigantische meteoriet
+`
+const nieuws2 = `
+### Lorem ipsum dolor sit amet, consectetur adipiscing elit
+**Datum**: 16 januari 2024  
+
+`
+const nieuws3 = `
+### Lorem ipsum dolor sit amet, consectetur adipiscing elit
+**Datum**: 9 januari 2024
+`
+
+document.getElementById('nieuws1').innerHTML = marked.parse(nieuws1);
+document.getElementById('nieuws2').innerHTML = marked.parse(nieuws2);
+document.getElementById('nieuws3').innerHTML = marked.parse(nieuws3);
