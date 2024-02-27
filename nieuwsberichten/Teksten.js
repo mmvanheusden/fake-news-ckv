6b3a227e0290489e9e6ab2918520b9fb7755251a
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2024-02-27 22:10:11 +0100

    v1.0.0

--- nieuwsberichten/Teksten.js
+++ nieuwsberichten/Teksten.js
@@ -1,18 +1,45 @@
-const nieuws1_voor = `
-# Gelekte beelden van NASA bewijzen dat de aarde plat is
+const nieuws1_1 = `
+# Gelekte simulaties van NASA bewijzen hoe dinosaurussen zijn uitgestorven
 **Datum**: 20 januari 2024  
 
 
 ---
-<img src="../kaasjam.jpg"
+<img src="./pics/1.gif"
      alt="NASA"
-     style="width: 100%" />
+     style="height: 50%" />
+<img src="./pics/2.gif"
+     alt="NASA"
+     style="height: 50%" />
 <br><br>
 
 `
 
-const nieuws1_na = `
-TEKST HIER XD
+const nieuws1_2 = `
+**In het hedendaagse leven hoor je altijd dat de aarde rond is, je hebt op school geleerd waarom en de logica erachter, Bijvoorbeeld de theorie van de zwaartekracht, of de theorie dat de aarde rond moet zijn omdat also het plat zou geweest zijn dan zou je amsterdam van utrecht zien, maar vandaag gaan we je bewijzen dat dit allemaal leugens zijn van de overheid om je onder hun controle te houden.**
+`
+const nieuws1_3 =`
+---
+**Bewijs in het dagelijks leven**  
+Ten eerste , Wie heeft de eerste fotos van de aarde gemaakt, Nasa, Nasa heeft zo veel macht, ze kunnen zeggen wat ze willen en iedereen zou het geloven, ze hebben controle over onze zicht van de ruimte, De zon en de maan draaien in cirkels boven de schijf. Seizoenen worden veroorzaakt doordat die cirkels de ene keer dichter bij de noordpool en dan weer verder ervan af zijn. De sterrenhemel is een glazen koepel. Zwaartekracht bestaat niet, en de afbeeldingen van NASA zijn allemaal trucage- of studiofoto's.<br><br>
+`
+const nieuws1_4 = `
+Ten tweede , Algemene manier hoe we naar de aarde kijken, kick naar de de logo van de Verenigde Naties (Zie afbeelding hiernaast) , als je naar hun logo kijkt zie je duidelijk dat de aarde plat afgebeeld is, ik bedoel kijk maar zelf, het is letterlijke bewijs dat de overheid ons manipuleert in het geloven dat de aarde rond is, Of als je naar de weerbericht kijkt is de aarde altijd PLAT afgebeeld, Zelfs de landkaart in je klaslokaal is een platte afbeelding<br><br>
+`
+const nieuws1_5 =`
+Wat is bijvoorbeeld de hoogste veronderstelde berg op aarde? [Everest](http://www.guinnessworldrecords.com/world-records/highest-mountain) . Maar dat is het niet. De naam is een weggeefactie; het is duidelijk een vorm van 'nooit rusten', want als je naar de grootste berg zou willen gaan, zou je letterlijk nooit rusten, omdat die er eigenlijk niet is. Hoe zit het met al die mensen [die hem hebben beklommen](http://www.telegraph.co.uk/news/worldnews/asia/mounteverest/10774877/Climbing-Mount-Everest-what-you-need-to-know.html) , zegt u? Denk er eens over na dat alle mensen die [gestorven zijn dit zogenaamd doen](https://en.wikipedia.org/wiki/List_of_people_who_died_climbing_Mount_Everest) . Hoe sterf je als je iets beklimt dat er niet is? Dat kun je niet. Ze werden duidelijk gedood om de samenzwering te beschermen, terwijl degenen die 'overleefden' bereid waren met de bal te spelen.<br><br>
+**Foto's vanuit de ruimte**
+
+Onze eerste Argument dat de aarde in ieder geval niet rond is omdat de maanlanding nep moet zijn, Ten eerste zijn er meerdere lichten van anderen hoeken te zien, dat kan toch niet, er zou alleen een licht moeten zijn en dat zou van de zon geweest zijn,
+Ook zijn er geen sterren te zijn op de maan, dat is raar vind je niet, want als je op aarde bent terwijl het donker is zie je de sterren in de lucht, de overheid moet toch wel een goede reden hebben om dit neppe maanlanding to maken, nou de reden dat ze dit hebben gedaan is omdat je in de fotos ook de aarde ziet, een ronde aarde, als alles van de maanlanding nep is geweest dan zou de ronde aarde ook nep zijn.<br><br>
+Iedereen kent de ISS, De satelliet waarvan we alle fotos van onze planeet zien, Meest Fotos zijn nep die van deze satelliet komen, ze zijn allemaal zo gemaakt dat het lijkt dat de aarde rond is, maar wij hebben een paar afbeelding die onbewerkt zijn gevonden die niet aangepast zijn, Op deze afbeeldingen hieronder zie je duidelijk dat de aarde plat is, the kleine curve dat je ziet is de rand van de platte aarde.<br><br>
+<img src="./pics/img_2.png" style="height: 33vh; display: block; margin: auto;">
+
+**School leert je leugens**
+
+Natuurlijk denkt u nu 'Maar hoe werkt de zwaartekracht en alle andere elementen die je op school hebt geleerd op een platte aarde, het kan niet werken' Nou het is erg simpel, het zijn allemaal leugens, [zie hier onze bron](https://theflatearthsociety.org/home/index.php/about-the-society/faq)<br><br>
+Zwaartekracht als theorie is onjuist. Voorwerpen vallen gewoon, In de platte-aarde gemeenschap bestaan ​​er verschillende theorieën over waarom dit gebeurt. Sommigen proberen dit uit te leggen met behulp van mechanica zoals elektromagnetisme, dichtheid of druk. Anderen maken gebruik van traditionele wiskunde, zoals het oneindige vlak model, en weer anderen gebruiken een nieuwe kijk op het probleem, zoals het niet-euclidische model, wat zeker is, is dat de zwaartekracht van de aarde op geen enkele manier houdbaar is.<br><br>
+**Kun je de kromming niet zien vanuit een vliegtuig?**![](RackMultipart20240227-1-2nwa0u_html_7fbd23756974053e.png) ![](RackMultipart20240227-1-2nwa0u_html_5dddb4c0157b092c.png) ![](RackMultipart20240227-1-2nwa0u_html_d2e774608096298a.jpg)  
+Nee. Zelfs als de aarde een bol zou zijn, zou dit een onmogelijkheid zijn. We hebben getuigenissen van verschillende piloten die zeggen dat er geen kromming te zien is op de hoogte van commerciële luchtvaartmaatschappijen. Er is gesuggereerd dat vliegtuigramen dienen om het zicht van passagiers te vervormen en hen te laten geloven dat ze krommingen zien, terwijl die er inderdaad niet zijn. Dit was waarschijnlijk een truc van vroege luchtvaartmaatschappijen om meer tickets te verkopen.
 
 `
 const nieuws2 = `
@@ -25,7 +52,10 @@ const nieuws3 = `
 **Datum**: 9 januari 2024
 `
 
-document.getElementById('nieuws1_voor').innerHTML = marked.parse(nieuws1_voor);
-document.getElementById('nieuws1_na').innerHTML = marked.parse(nieuws1_na);
+document.getElementById('nieuws1_1').innerHTML = marked.parse(nieuws1_1);
+document.getElementById('nieuws1_2').innerHTML = marked.parse(nieuws1_2);
+document.getElementById('nieuws1_3').innerHTML = marked.parse(nieuws1_3);
+document.getElementById('nieuws1_4').innerHTML = marked.parse(nieuws1_4);
+document.getElementById('nieuws1_5').innerHTML = marked.parse(nieuws1_5);
 document.getElementById('nieuws2').innerHTML = marked.parse(nieuws2);
 document.getElementById('nieuws3').innerHTML = marked.parse(nieuws3);
